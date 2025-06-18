38b52c9ed43b44d00fdac9d4942cd93e9042de9f
zowms <kianwfn@gmail.com>
2025-06-18 22:42:27 +0100

    main.js

--- config/main.js
+++ config/main.js
@@ -1,6 +1,6 @@
 // This changes the title of your site
 
-var sitename = "Google Classroom"; // Change this to change the name of your website.
+var sitename = "zowmgames"; // Change this to change the name of your website.
 var subtext = "v1.2"; // set the subtext
 
 // more settings in main.css
